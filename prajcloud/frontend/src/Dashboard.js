85fef38b8a65da6cacdc49992ba72f1b23f2d260
Aradhya1913 <nandeesharadhya19@gmail.com>
2025-08-28 20:11:08 +0530

    Update Dashboard.js

--- prajcloud/frontend/src/Dashboard.js
+++ prajcloud/frontend/src/Dashboard.js
@@ -4,7 +4,7 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import dataBanner from './assets/data.jpg';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const BASE_URL = 'https://existing-rabbit-martial-crossing.trycloudflare.com';
+const BASE_URL = 'https://additionally-empty-middle-outside.trycloudflare.com';
 
 function Dashboard({ userEmail }) {
   const [files, setFiles] = useState([]);
