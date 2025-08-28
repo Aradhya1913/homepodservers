8ff6647219c70c09be33b24632dd7e65e62c7f8b
Aradhya1913 <nandeesharadhya19@gmail.com>
2025-08-28 21:01:03 +0530

    Update Dashboard.js

--- prajcloud/frontend/src/Dashboard.js
+++ prajcloud/frontend/src/Dashboard.js
@@ -4,7 +4,7 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import dataBanner from './assets/data.jpg';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const BASE_URL = 'http://localhost:5001';
+const BASE_URL = "http://localhost:5001";
 
 function Dashboard({ userEmail }) {
   const [files, setFiles] = useState([]);
